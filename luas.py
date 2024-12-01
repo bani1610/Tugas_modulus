4ba192d6215bb0bd282e912a1782596600697e42
bani1610 <sholahuddinrq6@gmail.com>
2024-12-01 09:28:47 +0700

    Add files via upload

---
+++ luas.py
@@ -0,0 +1,18 @@
+import math 
+
+def persegi(s):
+    return s * s
+    
+def lingkaran(r):
+    return math.pi + (r ** 2)
+
+def segitiga(a, t):
+    return (a * t)/ 2
+
+def persegi_panjang(p, l):
+    return p * l
+
+def trapesium(a, b, t):
+    return ((a + b) * t) / 2
+    
+    
